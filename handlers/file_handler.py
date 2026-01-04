90d7cf2deaf259e3169e6f147fda2f4f149bfccf
dalamity <mrslime43@gmail.com>
2026-01-03 23:43:20 -0800

    Improve parsing performance

---
+++ handlers/file_handler.py
@@ -0,0 +1,108 @@
+from collections import defaultdict
+from concurrent.futures import ProcessPoolExecutor
+from datetime import datetime, timezone
+from dataclasses import dataclass
+from os import scandir, walk
+from os.path import join
+
+import os
+import exiftool
+
+
+@dataclass
+class Metadata:
+    ParsedDate: datetime
+    GPS: list[float, float]
+    Path: str
+
+
+class FileHandler:
+    def __init__(self, directory: str):
+        self.parent_directory: str = directory
+        self.files: list[str] = []
+
+        for dirpath, _, filenames in walk(directory):
+            for fname in filenames:
+                self.files.append(join(dirpath, fname))
+
+    def _parse_metadata_dates(self, metadata):
+        date_time: str = metadata.get("EXIF:DateTimeOriginal") or metadata.get(
+            "QuickTime:CreateDate"
+        )
+
+        date_time_offset: str = metadata.get("EXIF:OffsetTimeDigitized") or "+00:00"
+        if not date_time:
+            metadata["ParsedDate"] = None
+            return metadata
+
+        combined_date_time = (
+            f"{date_time.replace("T", " ").replace("Z", "")}{date_time_offset}"
+        )
+        try:
+            local_dt = datetime.strptime(combined_date_time, "%Y:%m:%d %H:%M:%S%z")
+            utc_dt = local_dt.astimezone(timezone.utc)
+            metadata["ParsedDate"] = utc_dt
+        except Exception:
+            metadata["ParsedDate"] = None
+
+        return metadata
+
+    def grab_metadata(self) -> list[Metadata]:
+        with exiftool.ExifToolHelper() as et:
+            metadatas = et.get_metadata(
+                self.files,
+                [
+                    # Original File
+                    "-SourceFile",
+                    # GPS Data
+                    "-Composite:GPSLatitude",
+                    "-Composite:GPSLongitude",
+                    # Date Data
+                    "-EXIF:DateTimeOriginal",
+                    "-QuickTime:CreateDate",
+                    "-EXIF:OffsetTimeDigitized",
+                ],
+            )
+
+        with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+            metadatas = list(executor.map(self._parse_metadata_dates, metadatas))
+
+        metadata_list = [
+            Metadata(
+                ParsedDate=m.get("ParsedDate"),
+                GPS=[
+                    m.get("Composite:GPSLatitude") or "0.0",
+                    m.get("Composite:GPSLongitude") or "0.0",
+                ],
+                Path=os.path.relpath(m.get("SourceFile"), self.parent_directory),
+            )
+            for m in metadatas
+        ]
+
+        return metadata_list
+
+    def _sort_metadata_by_date(self, metadata_list: list[Metadata]):
+        return sorted(metadata_list, key=lambda m: m.ParsedDate)
+
+    def _group_metadata_by_date(self, metadata_list: list[Metadata]):
+        metadata_group = defaultdict(list)
+        for metadata in metadata_list:
+            if metadata.ParsedDate is None:
+                continue
+            day = metadata.ParsedDate.date().isoformat()
+            metadata_group[day].append(metadata)
+
+        with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+            metadata_sorted = executor.map(
+                self._sort_metadata_by_date, metadata_group.values()
+            )
+
+        metadata_group = dict(sorted(zip(metadata_group.keys(), metadata_sorted)))
+        return metadata_group
+
+    def group_metadata(self, metadata_list, group_method):
+        match group_method:
+            case "date":
+                return self._group_metadata_by_date(metadata_list)
+            case _:
+                return None
--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from collections import defaultdict
 
-from handlers.photo_metadata_handler import PhotoMetadataHandler, Metadata
+from handlers.file_handler import FileHandler
 from handlers.folium_map_handler import FoliumMapHandler
 
-import os
-import webbrowser
-import re
 import tkinter
 from tkinter import filedialog
 
-photoHandler = PhotoMetadataHandler()
 renderHandler = FoliumMapHandler()
-
-
-def natural_sort(l):
-    # Stolen from https://stackoverflow.com/questions/4836710/is-there-a-built-in-function-for-string-natural-sort
-    convert = lambda text: int(text) if text.isdigit() else text.lower()
-    alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]
-    return sorted(l, key=alphanum_key)
 
 
 def folder_picker():
@@ -27,39 +16,15 @@ def folder_picker():
     return file_path
 
 
-def get_files_by_folder(root_dir):
-    result = {}
-
-    # Iterate only the first level of folders
-    top_level_folders = natural_sort(
-        [e for e in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, e))]
-    )
-
-    for folder_name in top_level_folders:
-        folder_path = os.path.join(root_dir, folder_name)
-        files = []
-
-        for dirpath, _, filenames in os.walk(folder_path):
-            for fname in filenames:
-                files.append(os.path.join(dirpath, fname))
-
-        result[folder_name] = files
-
-    return result
-
-
 def main():
     # Get Images
     directory = folder_picker()
-    folder_files = get_files_by_folder(directory)
-
-    # Get Metadata
-    metadata_by_folder: dict[str, list[Metadata]] = {}
-    for folder, files in folder_files.items():
-        metadata_by_folder[folder] = photoHandler.grab_metadata(files, directory)
+    file_handler = FileHandler(directory)
+    raw_metadata = file_handler.grab_metadata()
+    metadata_date_group = file_handler.group_metadata(raw_metadata, group_method="date")
 
     # Build The Map
-    for folder_name, file_metadata in metadata_by_folder.items():
+    for folder_name, file_metadata in metadata_date_group.items():
         gps_groups = defaultdict(list)
         for idx, metadata in enumerate(file_metadata):
             gps_groups[tuple(metadata.GPS)].append((idx, metadata))
@@ -73,8 +38,6 @@ def main():
     file_path = f"{directory}/map.html"
 
     renderHandler.render_map(file_path)
-
-    webbrowser.open(f"file://{os.path.abspath(file_path)}", new=2)
 
 
 if __name__ == "__main__":
