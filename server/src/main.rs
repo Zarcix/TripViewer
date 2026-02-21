a1b2c44f918e113ce54dad807c2c9c39524f7d80
dalamity <mrslime43@gmail.com>
2026-02-20 17:24:46 -0800

    Move config into toml

--- server/src/main.rs
+++ server/src/main.rs
@@ -41,33 +41,7 @@ fn setup() -> Result<(), String> {
     Ok(())
 }
 
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{Request, Response};
-
 pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    // Fairing to remove localhost limitations for API testing
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, PUT, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-        response.remove_header("X-Frame-Options");
-    }
-}
 
 #[options("/<_..>")]
 fn all_options() {
@@ -85,7 +59,6 @@ fn run_server() -> _ {
         .unwrap();
 
     rocket::build()
-        .attach(CORS)
         .mount("/api/photoset", photoset_api::api_routes::route_list())
         .mount("/api/photo", photo_api::api_routes::route_list())
         .mount("/photos", file_server::api_routes())
