ebe952a97afa9590f6c0f0476c7111531193ec5e
dalamity <mrslime43@gmail.com>
2026-02-21 19:55:07 -0800

    Update headers

--- src/main.rs
+++ src/main.rs
@@ -59,10 +59,7 @@ impl Fairing for CORS {
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, PUT, OPTIONS, DELETE",
-        ));
+        response.set_header(Header::new("Access-Control-Allow-Methods","*"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
         response.remove_header("X-Frame-Options");
